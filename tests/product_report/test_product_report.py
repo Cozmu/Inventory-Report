e1da20ed32794905a000765fa2f0f595a4c28dfa
Cozmu <www.jorgereis@gmail.com>
2023-07-14 14:08:59 -0300

    fix: corrigindo erro de lint

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -3,15 +3,16 @@ from inventory_report.product import Product
 
 def test_product_report() -> None:
     instance_product = Product(
-        '1', 'carne', 'friboi', '10/01/2019', '10/05/2020', '123123', 'embalado'
+        '1', 'carne', 'friboi', '10/01/2019', '10/05/2020', '123123',
+        'embalado'
     )
 
     assert instance_product.__str__() == (
-        f'The product 1 - carne '
-        f'with serial number 123123 '
-        f'manufactured on 10/01/2019 '
-        f'by the company friboi '
-        f'valid until 10/05/2020 '
-        f'must be stored according to the following instructions: '
-        f'embalado.'
+        'The product 1 - carne '
+        'with serial number 123123 '
+        'manufactured on 10/01/2019 '
+        'by the company friboi '
+        'valid until 10/05/2020 '
+        'must be stored according to the following instructions: '
+        'embalado.'
     )
