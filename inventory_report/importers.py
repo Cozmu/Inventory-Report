c28bb5a2595f6b3a54181b895b1b44d3ab050f8a
Cozmu <www.jorgereis@gmail.com>
2023-07-14 15:23:35 -0300

    feat: implementando classe JsonImporter (req 4)

--- inventory_report/importers.py
+++ inventory_report/importers.py
@@ -1,3 +1,4 @@
+import json
 from abc import ABC, abstractmethod
 from typing import Dict, Type
 
@@ -13,8 +14,25 @@ class Importer(ABC):
         ...
 
 
-class JsonImporter:
-    pass
+class JsonImporter(Importer):
+    def import_data(self) -> list[Product]:
+        with open(self.path, "r") as file:
+            data = json.load(file)
+
+        result = []
+        for item in data:
+            product = Product(
+                item["id"],
+                item["product_name"],
+                item["company_name"],
+                item["manufacturing_date"],
+                item["expiration_date"],
+                item["serial_number"],
+                item["storage_instructions"],
+            )
+            result.append(product)
+
+        return result
 
 
 class CsvImporter:
