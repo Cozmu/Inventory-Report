e22e0f0e9f166cd8440d3129c79b24953c896647
Cozmu <www.jorgereis@gmail.com>
2023-07-14 20:35:39 -0300

    feat: começo da implementação do metodo generate da classe SimpleReport

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,22 @@
+from datetime import date
+
+from inventory_report.inventory import Inventory
+from inventory_report.product import Product
+
+
 class SimpleReport:
-    pass
+    def __init__(self) -> None:
+        self.stock: list[Inventory] = []
+
+    def add_inventory(self, inventory: Inventory) -> None:
+        self.stock.append(inventory)
+
+    def generate(self) -> str:
+        for inventory in self.stock:
+            sorted_old = 
+
+        return (
+            f"Oldest manufacturing date: {}"
+            f"Closest expiration date: {}"
+            f"Company with the largest inventory: {}"
+        )
