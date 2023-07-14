f2c094126de42cb4378e8ca570e969f959f77249
Cozmu <www.jorgereis@gmail.com>
2023-07-13 21:48:58 -0300

    test: implementando testes do metodo __str__ da classe Product (req 2)

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,2 +1,17 @@
+from inventory_report.product import Product
+
+
 def test_product_report() -> None:
-    raise NotImplementedError
+    instance_product = Product(
+        '1', 'carne', 'friboi', '10/01/2019', '10/05/2020', '123123', 'embalado'
+    )
+
+    assert instance_product.__str__() == (
+        f'The product 1 - carne '
+        f'with serial number 123123 '
+        f'manufactured on 10/01/2019 '
+        f'by the company friboi '
+        f'valid until 10/05/2020 '
+        f'must be stored according to the following instructions: '
+        f'embalado.'
+    )
