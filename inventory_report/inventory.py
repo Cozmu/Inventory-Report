8707356d4a95161a11eee721961a285d07d5fb18
Cozmu <www.jorgereis@gmail.com>
2023-07-14 17:19:28 -0300

    feat: adicionando protocolo Report (req 6). Inventory tambem implementando mas com defeito

--- inventory_report/inventory.py
+++ inventory_report/inventory.py
@@ -1,2 +1,16 @@
+from typing import Optional
+
+from inventory_report.product import Product
+
+
 class Inventory:
-    pass
+    def __init__(self, data:Optional[list[Product]] = None) -> None:
+        self._data = data or []
+
+    @property
+    def data(self) -> Optional[list[Product]]:
+        return self._data
+
+    def add_data(self, new_product:list[Product]) -> None:
+        for product in new_product:
+            self._data.append(product)
